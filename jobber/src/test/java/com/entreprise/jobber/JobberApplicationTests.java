9e884d76cfb1850044cbfabfaf6c0b95e699e18a
dmalembe <malembedidier24@gmail.com>
2021-07-05 15:56:23 +0200

    Init Backend project

---
+++ jobber/src/test/java/com/entreprise/jobber/JobberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entreprise.jobber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
